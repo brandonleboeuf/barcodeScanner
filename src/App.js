ae41e940858c2855865c5f650b2e4099e76a2466
Brandon LeBoeuf <BrandonL@henry-v.com>
2021-09-24 18:24:40 -0700

    removes dependency array

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   useEffect(() => {
     getAuthToken()
     getData()
-  }, [aventriAccessToken, aventriMessage])
+  })
 
   const getAuthToken = async () => {
     const formdata = new FormData()
